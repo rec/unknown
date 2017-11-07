90c9b52738ba7436a5d8d58c6c8da5582e38b9e4
Tom Ritchford <tom@swirly.com>
2017-11-07 21:17:32 +0100

    Refactor sound_source_test.py

--- test/sound_source_test.py
+++ test/sound_source_test.py
@@ -1,23 +1,28 @@
 import unittest
 from unknown import sound_source
 
-MOCK_FILES = {
-    'simple.wav': [
-        [0x0000, 0x0000],
-    ],
-    'biramp.wav': [
-        [0x0000, 0xFFFF],
-        [0x4000, 0xAFFF],
-        [0x8000, 0x7FFF],
-        [0xB000, 0x3FFF],
-    ],
-}
-
 
 class MockWave:
+    MOCK_FILES = {
+        'simple.wav': (
+            (0x0000, 0x0000),
+        ),
+        'biramp.wav': (
+            (0x0000, 0xFFFF),
+            (0x1000, 0xEFFF),
+            (0x2000, 0xDFFF),
+            (0x3000, 0xCFFF),
+            (0x4000, 0xBFFF),
+            (0x5000, 0xAFFF),
+            (0x6000, 0x9FFF),
+            (0x7000, 0x8FFF),
+            (0x8000, 0x7FFF),
+        ),
+    }
+
     def __init__(self, name):
         as_bytes = []
-        for l, r in MOCK_FILES[name]:
+        for l, r in self.MOCK_FILES[name]:
             as_bytes.extend(reversed(divmod(r, 256) + divmod(r, 256)))
         self.frames = bytes(as_bytes)
 
@@ -34,12 +39,16 @@ class MockWave:
         pass
 
 
-def SoundSource(name, fade_frames=0):
-    return sound_source.SoundSource(name, fade_frames, MockWave)
+class SoundSourceTest(unittest.TestCase):
+    def assert_frames(self, name, *expected, fade_frames=0):
+        source = sound_source.SoundSource(name, fade_frames, MockWave)
+        actual = []
+        while True:
+            frame = source.next_frame()
+            if not frame:
+                break
+            actual.append(frame)
+        self.assertEqual(list(expected), actual)
 
-
-class ImportAllTest(unittest.TestCase):
     def test_simple(self):
-        source = SoundSource('simple.wav')
-        self.assertEqual(source.next_frame(), (0x0000, 0x0000))
-        self.assertFalse(source.next_frame())
+        self.assert_frames('simple.wav', (0x0000, 0x0000))
