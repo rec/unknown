7814fb99960bfa809511f0db0858aece6dd88ce5
Tom Ritchford <tom@swirly.com>
2017-11-15 13:38:54 +0100

    Start of better tests for rotator

--- test/unknown/source_rotator_test.py
+++ test/unknown/source_rotator_test.py
@@ -21,65 +21,73 @@ class RotateSourcesTest(unittest.TestCase):
         if expected != actual:
             print('compare FAILED')
             print(*['        0x%04x,' % a for a in actual], sep='\n')
-        False and self.assertEqual(expected, actual)
+        self.assertEqual(expected, actual)
+
+    def combine_compare(self, files, fade, speed, expected):
+        ins = [mocks.combine(fade, f, f) for f in files]
+        outs = MockOutput(), MockOutput(), MockOutput(), MockOutput()
+        speed *= source_rotator.FRAMES_PER_MINUTE
+        source_rotator.source_rotator(ins, outs, speed)
+        for expected_actual in zip(expected, (o.results for o in outs)):
+            self.compare(*expected_actual)
+        self.assertEqual(len(expected), 4)
 
     def test_rotate_dc(self):
-        in1 = mocks.combine(3, 'dc.wav', 'dc.wav')
-        in2 = mocks.combine(3, 'dc.wav', 'dc.wav')
-        outs = MockOutput(), MockOutput(), MockOutput()
-        speed = source_rotator.FRAMES_PER_MINUTE / 3
-        source_rotator.source_rotator((in1, in2), outs, speed)
-        self.compare((), outs[0].results)
-        self.compare((), outs[1].results)
-        self.compare((), outs[2].results)
-        if not True:
-            raise ValueError
+        files = ['dc2.wav', 'dc2.wav']
+        self.combine_compare(files, 0, 0, ROTATE_DC)
+        self.combine_compare(files, 0, 1 / 4, ROTATE_DC)
+        self.combine_compare(files, 0, 1 / 3, ROTATE_DC)
+        self.combine_compare(files, 0, 1, ROTATE_DC)
+        self.combine_compare(files, 0, 3, ROTATE_DC)
+        self.combine_compare(files, 0, 4, ROTATE_DC)
 
+    def test_fade_dc2(self):
+        files = ['dc2.wav', 'dc2.wav']
+        self.combine_compare(files, 2, 0, FADE_DC2)
+        self.combine_compare(files, 2, 1 / 4, FADE_DC2)
+        self.combine_compare(files, 2, 1 / 3, FADE_DC2)
+        self.combine_compare(files, 2, 1, FADE_DC2)
+        self.combine_compare(files, 2, 3, FADE_DC2)
+        self.combine_compare(files, 2, 4, FADE_DC2)
+
+    def test_fade_dc3(self):
+        files = ['dc2.wav', 'dc2.wav']
+        self.combine_compare(files, 3, 0, FADE_DC3)
+        self.combine_compare(files, 2, 1 / 4, FADE_DC2)
+        self.combine_compare(files, 2, 1 / 3, FADE_DC2)
+        self.combine_compare(files, 2, 1, FADE_DC2)
+        self.combine_compare(files, 2, 3, FADE_DC2)
+        self.combine_compare(files, 2, 4, FADE_DC2)
+
+    def test_rotate_saw(self):
+        files = ['saw.wav', 'saw.wav']
+        self.combine_compare(files, 0, 0, ROTATE_SAW)
+
+
+ROTATE_SAW = (
+    (0x0000, 0x0880, 0x1100, 0x1980, 0x0000, 0x0880, 0x1100, 0x1980),
+    (0x0000, 0x0880, 0x1100, 0x1980, 0x0000, 0x0880, 0x1100, 0x1980),
+    (0x0000, 0x0880, 0x1100, 0x1980, 0x0000, 0x0880, 0x1100, 0x1980),
+    (0x0000, 0x0880, 0x1100, 0x1980, 0x0000, 0x0880, 0x1100, 0x1980),
+)
 
 ROTATE_DC = (
-    (
-        0x0000,
-        0x0000,
-        0x0000,
-        0x0000,
-        0x0000,
-        0x0000,
-        0x0000,
-        0x0000,
-        0x0000,
-        0x0000,
-        0x0000,
-        0x0000,
-        0x0000,
-    ),
-    (
-        0x0000,
-        0x5001,
-        0x9fff,
-        0xf000,
-        0xf000,
-        0xf000,
-        0xf000,
-        0xf000,
-        0xf000,
-        0xf000,
-        0xf000,
-        0x9fff,
-        0x5001,
-    ),
-    (
-        0x0000,
-        0x1aab,
-        0x3555,
-        0x5000,
-        0x5000,
-        0x5000,
-        0x5000,
-        0x5000,
-        0x5000,
-        0x5000,
-        0x5000,
-        0x3555,
-        0x1aab,
-    ),
+    (0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000),
+    (0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000),
+    (0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000),
+    (0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000),
+)
+
+FADE_DC2 = (
+    (0x0000, 0x0800, 0x1000, 0x1000, 0x1000, 0x0800),
+    (0x0000, 0x0800, 0x1000, 0x1000, 0x1000, 0x0800),
+    (0x0000, 0x0800, 0x1000, 0x1000, 0x1000, 0x0800),
+    (0x0000, 0x0800, 0x1000, 0x1000, 0x1000, 0x0800),
+)
+
+FADE_DC3 = (
+    (0x0000, 0x0555, 0x1000, 0x1000, 0x0555),
+    (0x0000, 0x0555, 0x1000, 0x1000, 0x0555),
+    (0x0000, 0x0555, 0x1000, 0x1000, 0x0555),
+    (0x0000, 0x0555, 0x1000, 0x1000, 0x0555),
 )
--- unknown/source_rotator.py
+++ unknown/source_rotator.py
@@ -3,9 +3,8 @@ from . import constants, rotations
 FRAMES_PER_MINUTE = 60 * constants.FRAMERATE
 
 
-def source_rotator(
-        ins, outs, speeds, in_rotations=None, out_rotations=None,
-        left_right_offset=0.5):
+def source_rotator(ins, outs, speeds,
+                   in_rotations=None, out_rotations=None, stereo_spread=None):
     """
     rotation units are 1.0 == 360 degrees
     Arguments:
@@ -18,6 +17,9 @@ def source_rotator(
     in_rotations = in_rotations or [i / len(ins) for i in range(len(ins))]
     out_rotations = out_rotations or [i / len(outs) for i in range(len(outs))]
     out_rotations = rotations.Rotations(out_rotations)
+    if stereo_spread is None:
+        stereo_spread = 1 / len(outs)
+    spread = stereo_spread / 2
 
     # Convert speeds from rpms to rotations per frame
     try:
@@ -32,21 +34,26 @@ def source_rotator(
         print()
         print(frame_index)
         print('source_rotator frames', [pf(f) for f in frames])
+
         out_frames = [0] * len(outs)
         for frame, speed, rotation in zip(frames, speeds, in_rotations):
             left, right = frame
             rotation += frame_index * speed
-            for channel, offset in (left, 0), (right, left_right_offset):
+            for channel, offset in (left, -spread), (right, spread):
                 index, ratio = out_rotations.find(rotation + offset)
+
                 print('rotating', index, '%4.2f' % ratio,
                       hex(channel), '%5.3f' % rotation, offset)
                 print('%5.3f' % (ratio * channel),
                       '%5.3f' % ((1 - ratio) * channel))
+
                 out_frames[index] += ratio * channel
                 out_frames[index + 1] += (1 - ratio) * channel
 
         for i, (sample, out) in enumerate(zip(out_frames, outs)):
             round_sample = min(round(sample), 65535)
-            result = tuple(reversed(divmod(round_sample, 256)))
-            print('source_rotator', i, hex(round_sample), result)
-            out.writeframes(result)
+            frames = tuple(reversed(divmod(round_sample, 256)))
+
+            print('source_rotator', i, hex(round_sample), frames)
+
+            out.writeframes(frames)
